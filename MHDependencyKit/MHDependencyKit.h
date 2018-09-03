22773b292394cb270bfebf6eae5dd7005639b9ea
Milen Halachev <milenski88@gmail.com>
2018-09-03 11:40:05 +0300

    Initial Commit

---
+++ MHDependencyKit/MHDependencyKit.h
@@ -0,0 +1,19 @@
+//
+//  MHDependencyKit.h
+//  MHDependencyKit
+//
+//  Created by Milen Halachev on 3.09.18.
+//  Copyright © 2018 KoCMoHaBTa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHDependencyKit.
+FOUNDATION_EXPORT double MHDependencyKitVersionNumber;
+
+//! Project version string for MHDependencyKit.
+FOUNDATION_EXPORT const unsigned char MHDependencyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHDependencyKit/PublicHeader.h>
+
+

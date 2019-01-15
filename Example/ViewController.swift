6c83e387d640f6c44a9532ac48eb31ae10976359
Milen Halachev <milenski88@gmail.com>
2019-01-15 15:01:16 +0200

    - setup empty example target

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Milen Halachev on 15.01.19.
+//  Copyright © 2019 KoCMoHaBTa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

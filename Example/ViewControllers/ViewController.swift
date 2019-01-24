5c17eed08b0bdaf18709bab82ec7a4e58874088a
Milen Halachev <milenski88@gmail.com>
2019-01-24 15:14:41 +0200

    - added more examples

--- Example/ViewControllers/ViewController.swift
+++ Example/ViewControllers/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import MHDependencyKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func manualStoryboard() {
+        
+        self.performSegue(withIdentifier: "push", sender: nil)
+    }
+    
+    @IBAction func manualConfiguredStoryboard() {
+        
+        self.performSegue(withIdentifier: "modal", sender: nil) { (_, destination: AppVersionConfigurable) in
+            
+            destination.appVersion = "this custom configured app version 111"
+        }
+    }
+    
+    @IBAction func manualResolve() {
+        
+        let controller = self.storyboard!.instantiateViewController(withIdentifier: "InputNavigationController")
+        self.dependencyCoordinator.resolveDependencies(from: self, to: controller)
+        self.present(controller, animated: true, completion: nil)
     }
 }
 
